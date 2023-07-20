d7a6bd06ad3684380e6e40c9152a02c2094f9330
Mete <mete.ulken71@gmail.com>
2023-07-20 21:38:37 +0300

    if else yapilari

---
+++ if_else/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//if else
+
+
+int time = DateTime.Now.Hour;
+
+if(time < 12)
+{
+    Console.WriteLine("Good Morning");
+}
+else if(time < 18)
+{
+    Console.WriteLine("Good Afternoon");
+}
+else
+{
+    Console.WriteLine("Good Evening");
+}
+
+string sonuc = time <= 18 ? "Günaydin" : "iyi Aksamlar";
+sonuc = time == 18 ? "iyi Aksamlar" : time < 12 ? "Günaydin" : "iyi Geceler";
+
+Console.WriteLine(sonuc);
+
