a11595a21e628fb6c24b0901f07357a0a769351d
Mete <mete.ulken71@gmail.com>
2023-08-03 11:10:20 +0300

    .

--- enum/Program.cs
+++ enum/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// enum (enumeration) -> belirli bir veri tipinin değişkenlerini tanımlamak için kullanılır.
+
+// enum tanımlama
+
+enum Days
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
