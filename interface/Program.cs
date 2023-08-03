43e7e5b148ba487dbfd398c61a83da8fc39f74e6
Mete <mete.ulken71@gmail.com>
2023-08-03 15:12:14 +0300

    .

---
+++ interface/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// Interface nedir? Interface'ler, birbirine benzeyen ama farklı işlevleri olan sınıflar için kullanılır.
+// Interface'ler, içerisinde sadece imza bulunan, içi boş metodlardır. İmza, metodun adı, parametreleri ve dönüş tipinden oluşur.
+
+public interface ILogger
+{
+    void WriteLog();
+    void WriteLog2();
+}
