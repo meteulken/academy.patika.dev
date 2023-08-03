23ee334b036bf6a4377f41f067245c15a05d47a4
Mete <mete.ulken71@gmail.com>
2023-08-03 19:08:57 +0300

    .

---
+++ abstract_class/Civic.cs
@@ -0,0 +1,20 @@
+namespace otomobils
+{
+    public class Civic : IOtomobil
+    {
+        public Marka HangiMarkaninAraci()
+        {
+            return Marka.Honda;
+        }
+
+        public int KacTekerlektenOlusur()
+        {
+            return 4;
+        }
+
+        public Renk StandartRengiNe()
+        {
+            return Renk.Gri;
+        }
+    }
+}
---
+++ abstract_class/IOtomobil.cs
@@ -0,0 +1,9 @@
+namespace otomobils
+{
+    public interface IOtomobil
+    {
+        int KacTekerlektenOlusur();
+        Marka HangiMarkaninAraci();
+        Renk StandartRengiNe();
+    }
+} 
