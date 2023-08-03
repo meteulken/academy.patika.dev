512d51f655af117eaea9bb8cce25107f6b89013a
Mete <mete.ulken71@gmail.com>
2023-08-03 10:51:25 +0300

    structs

---
+++ structs/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// struct nedir ?
+// struct yapısı, değer tipinde bir veri tutmak için kullanılır.
+// struct yapısı ile oluşturulan bir nesne stack'te tutulur.
+
+// class nedir ?
+// class yapısı, referans tipinde bir veri tutmak için kullanılır.
+// class yapısı ile oluşturulan bir nesne heap'te tutulur.
+// class yapısı ile oluşturulan bir nesne new anahtar kelimesi ile oluşturulur.
+
+Dikdortgen dikdortgen = new Dikdortgen();
+dikdortgen.KisaKenar = 3;
+dikdortgen.UzunKenar = 4;
+
+Console.WriteLine("Class Alan Hesapla :{0}",dikdortgen.AlanHesapla());
+
+DikdortgenStruct dikdortgenStruct;
+
+dikdortgenStruct.KisaKenar = 3;
+dikdortgenStruct.UzunKenar = 4;
+
+Console.WriteLine("Struct Alan Hesapla :{0}",dikdortgenStruct.AlanHesapla());
+
+
+class Dikdortgen
+{
+    public int KisaKenar { get; set; }
+    public int UzunKenar { get; set; }
+
+    public long AlanHesapla()
+    {
+        return this.KisaKenar * this.UzunKenar;
+    }
+}
+
+struct DikdortgenStruct
+{
+    public int KisaKenar;
+    public int UzunKenar;
+
+    public long AlanHesapla()
+    {
+        return this.KisaKenar * this.UzunKenar;
+    }
+}
