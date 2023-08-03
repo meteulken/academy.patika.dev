854ca1f4079509ade60922c4aafa95f441999ebb
Mete <mete.ulken71@gmail.com>
2023-08-03 11:19:34 +0300

    .

--- enum/Program.cs
+++ enum/Program.cs
@@ -19,3 +19,24 @@ enum Days
 // enum kullanımı
 
 Days day = Days.Monday;
+
+Console.WriteLine(day);
+
+
+// enum içerisindeki değerleri değiştirme
+
+enum Months
+{
+    January = 1,
+    February = 2,
+    March = 3,
+    April = 4,
+    May = 5,
+    June = 6,
+    July = 7,
+    August = 8,
+    September = 9,
+    October = 10,
+    November = 11,
+    December = 12
+}
